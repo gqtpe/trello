4e1b45e28bf3524eac8d15323eb09cef9a35f598
gqtpe <work.temirtas@gmail.com>
2024-05-29 20:01:42 +0500

    todolist v03(addTask, eventHandlers)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./components/TodoList/TodoList";
+import {v4 as uuid} from "uuid";
 
 export type FilterTypeValuesType = "ACTIVE" | "ALL" | "COMPLETED"
 
 function App() {
     let [tasks, setTasks] = useState<TaskType[]>([
-        {id: 1, title: "CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false},
-        {id: 4, title: "React Native", isDone: false},
+        {id: uuid(), title: "CSS", isDone: true},
+        {id: uuid(), title: "JS", isDone: true},
+        {id: uuid(), title: "React", isDone: false},
+        {id: uuid(), title: "React Native", isDone: false},
     ])
     let [filter, setFilter] = useState<FilterTypeValuesType>("ALL")
-    const removeTask = (id: number) => {
+    const removeTask = (id: string) => {
         let filteredTasks = tasks.filter(t => t.id !== id)
         setTasks(filteredTasks)
     }
-    const changeFilter = (filter: FilterTypeValuesType) =>{
+    const changeFilter = (filter: FilterTypeValuesType) => {
         setFilter(filter)
     }
+    const addTask = (title: string) => {
+        let newTask: TaskType = {id: uuid(), title: title, isDone: false}
+        let newTasks: TaskType[] = [newTask, ...tasks]
+        setTasks(newTasks)
+    }
     let resultTasks = tasks
-    if(filter === "ACTIVE"){
-        resultTasks = resultTasks.filter(t=>!t.isDone)
+    if (filter === "ACTIVE") {
+        resultTasks = resultTasks.filter(t => !t.isDone)
 
     }
-    if(filter === "COMPLETED"){
-        resultTasks = resultTasks.filter(t=>t.isDone)
+    if (filter === "COMPLETED") {
+        resultTasks = resultTasks.filter(t => t.isDone)
     }
+    console.log(tasks)
     return (
         <div className="App">
             <Todolist
@@ -34,6 +41,7 @@ function App() {
                 tasks={resultTasks}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
+                addTask={addTask}
             />
         </div>
     );
