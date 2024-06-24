7eefdb08f185a2a9372f4828f2eef28b1dcf0d3b
gqtpe <work.temirtas@gmail.com>
2024-06-24 08:35:41 +0500

    todoList v10:replaced rendering App

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppWithReducer from "./AppWithReducer";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <AppWithReducer/>
 );
 
 reportWebVitals();
