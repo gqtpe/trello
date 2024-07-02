195b66f1151a74e4297474b5b68c184b94c970b8
gqtpe <work.temirtas@gmail.com>
2024-07-02 09:22:28 +0500

    todoList v13: created custom hook useAddItemForm

---
+++ src/components/AddItemForm/hooks/useAddItemForm.ts
@@ -0,0 +1,28 @@
+import React, {KeyboardEvent, useState} from "react";
+import {ErrorType} from "../../../app/AppWithRedux";
+
+export const useAddItemForm = (onItemAdded: (title: string)=>void) =>{
+    let [title, setTitle] = useState<string>('');
+    let [error, setError] = useState<ErrorType>(null)
+
+    const addItem = () => {
+        if (title.trim() !== '') {
+            onItemAdded(title)
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+    const enterKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            addItem()
+        }
+    }
+    const changeItemHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (error) {
+            setError(null)
+        }
+        setTitle(e.currentTarget.value)
+    }
+    return {title, error, changeItemHandler, enterKeyPressHandler,addItem}
+}
