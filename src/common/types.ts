6c5f4163d2e2e1dfc523030b9abb7bc2636ac68b
gqtpe <work.temirtas@gmail.com>
2024-07-03 12:04:32 +0500

    todoList v13.2: new TaskTypes, TodoListTypes

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,11 +1,36 @@
 export type ErrorType = string | null
 export type FilterTypeValuesType = "ACTIVE" | "ALL" | "COMPLETED"
+
 export type TodoListType = {
-    id: string,
-    title: string,
-    filter: FilterTypeValuesType
+    id: string
+    title: string
+    addedDate: string
+    order: number
+}
+export type TaskType = {
+    description: string
+    title: string
+    status: TaskStatuses
+    priority: TaskPriorities
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+}
+export type TasksStateType = { [key: string]: TaskType[] }
+export enum TaskStatuses {
+    New,
+    InProgress,
+    Completed,
+    Draft
 }
 
-
-
-export type TaskType = { id: string, title: string, isDone: boolean }
+export enum TaskPriorities {
+    Low,
+    Middle,
+    High,
+    Urgently,
+    Later
+}
