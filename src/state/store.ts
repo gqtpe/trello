1db463821b746ba3f58cec94530d339dcb0bedc9
gqtpe <work.temirtas@gmail.com>
2024-07-04 12:27:53 +0500

    todoList v14: apply middleWare for thunk processing

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,15 @@
-import {combineReducers, legacy_createStore} from 'redux'
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
 import {tasksReducer} from "./tasks-reducer";
 import {todoListsReducer} from "./todoLists-reducer";
+import {thunk, ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
+
 
 const rootReducer = combineReducers({
     todoLists: todoListsReducer,
     tasks: tasksReducer,
 })
-
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, undefined, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
