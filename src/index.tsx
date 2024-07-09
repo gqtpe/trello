d9904790f870a0cb25358d3000aa6afbde152ae0
gqtpe <work.temirtas@gmail.com>
2024-07-09 13:19:46 +0500

    todoList v15: refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
-import AppWithRedux from "./app/App";
+import App from "./app/App";
 import {Provider} from "react-redux";
-import {store} from "./state/store";
+import {store} from "./app/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>
 );
 
