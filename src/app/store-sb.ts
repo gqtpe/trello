c395e7a41836955dcac41e47f9a2b4ff18fd4c17
gqtpe <work.temirtas@gmail.com>
2024-07-09 13:12:28 +0500

    todoList v15: store for storybook

---
+++ src/app/store-sb.ts
@@ -0,0 +1,130 @@
+import {applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import {todoListsReducer} from "../features/TodoListsList/TodoList/todoLists-reducer";
+import {tasksReducer} from "../features/TodoListsList/TodoList/tasks-reducer";
+import {appReducer} from "./app-reducer";
+import {AppRootStateType} from "./store";
+import {TaskPriorities, TaskStatuses} from "../common/types";
+import {thunk} from "redux-thunk";
+
+const rootReducer = combineReducers({
+    todoLists: todoListsReducer,
+    tasks: tasksReducer,
+    app: appReducer,
+})
+const initialGlobalState: AppRootStateType = {
+        app: {
+            error: null,
+            status: 'idle',
+        },
+        tasks: {
+            ['todolistID1']: [
+                {
+                    id: '1',
+                    title: 'CSS',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.New,
+                    todoListId: 'todolistID1'
+                },
+                {
+                    id: '2',
+                    title: 'React',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.Completed,
+                    todoListId: 'todolistID1'
+                },
+                {
+                    id: '3',
+                    title: 'TS',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.New,
+                    todoListId: 'todolistID1'
+                },
+                {
+                    id: '4',
+                    title: 'Next',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.Completed,
+                    todoListId: 'todolistID1'
+                },
+            ],
+            ['todolistID2']: [
+                {
+                    id: '1',
+                    title: 'wakeup',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.Completed,
+                    todoListId: 'todolistID2'
+                },
+                {
+                    id: '2',
+                    title: 'do important',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.New,
+                    todoListId: 'todolistID2'
+                },
+                {
+                    id: '3',
+                    title: 'code',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.New,
+                    todoListId: 'todolistID2'
+                },
+                {
+                    id: '4',
+                    title: 'Next',
+                    addedDate: '',
+                    deadline: '',
+                    description: '',
+                    order: 0,
+                    priority: TaskPriorities.Low,
+                    startDate: '',
+                    status: TaskStatuses.Completed,
+                    todoListId: 'todolistID2'
+                },
+            ]
+        },
+        todoLists: [
+            {id: 'todolistID1', title: "What to learn", entityStatus: 'idle', filter: "ACTIVE", addedDate: '', order: -1},
+            {id: 'todolistID2', title: "What to do", entityStatus: 'loading', filter: "ALL", addedDate: '', order: 0},
+        ]
+    }
+
+;
+
+// @ts-ignore
+export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState, applyMiddleware(thunk))
