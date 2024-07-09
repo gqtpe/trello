f17ddec91a0fec5ea3bc17c0bf4a5a7beb232d9e
gqtpe <work.temirtas@gmail.com>
2024-07-09 13:19:14 +0500

    todoList v15: error alerts block(connected with redux)

---
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,25 @@
+import {Alert, Snackbar} from "@mui/material";
+import React from "react";
+import {useSelector} from "react-redux";
+import {AppRootStateType, useAppDispatch} from "../../app/store";
+import {ErrorType} from "../../common/types";
+import {setAppErrorAC} from "../../app/app-reducer";
+
+
+export const ErrorSnackbar = () => {
+    const error = useSelector<AppRootStateType, ErrorType>(state => state.app.error)
+    const dispatch = useAppDispatch()
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        dispatch(setAppErrorAC(null))
+    };
+
+    return <Snackbar open={error !== null} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
+            {error}
+        </Alert>
+    </Snackbar>
+}
