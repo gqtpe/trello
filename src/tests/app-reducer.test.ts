e82882697e8f38e4f1fad8399d177887e6c46e91
gqtpe <work.temirtas@gmail.com>
2024-07-09 13:07:07 +0500

    todoList v15: tests for setAppStatus, setAppError

---
+++ src/tests/app-reducer.test.ts
@@ -0,0 +1,24 @@
+import {appReducer, RequestStatusType, setAppErrorAC, setAppStatusAC, StateType} from "../app/app-reducer";
+import {ErrorType} from "../common/types";
+
+
+let startState: StateType
+beforeEach(() => {
+    startState = {
+        status: 'idle',
+        error: null
+    }
+})
+test('app reducer should set app status', () => {
+    const status: RequestStatusType = 'succeeded'
+    const endState = appReducer(startState, setAppStatusAC(status))
+
+    expect(endState.status).toBe(status)
+
+})
+test('app reducer should set app error', () => {
+    const error: ErrorType = 'some error'
+    const endState = appReducer(startState, setAppErrorAC(error))
+
+    expect(endState.error).toBe(error)
+})
