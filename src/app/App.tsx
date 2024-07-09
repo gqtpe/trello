d8f830e29104926126d3bf2e742f020383e32edb
gqtpe <work.temirtas@gmail.com>
2024-07-09 13:19:28 +0500

    todoList v15: demo property added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.scss';
-import {AppBar, IconButton, Toolbar} from "@mui/material";
+import {AppBar, IconButton, LinearProgress, Toolbar} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import {ThemeProvider} from '@mui/material/styles'
 import Switch from '@mui/material/Switch'
 import CssBaseline from '@mui/material/CssBaseline'
 import {useTheme} from "./hooks/useTheme";
 import TodoListsList from "../features/TodoListsList/TodoListsList";
-
-function App() {
+import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+type PropsType = {
+    demo?:boolean
+}
+function App({demo = false}:PropsType) {
     console.log('App is called')
-    const {theme, changeThemeHandler} = useTheme()
+    const {theme, changeThemeHandler,status} = useTheme()
     return (
         <div className="App">
+            <ErrorSnackbar/>
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
                 <AppBar position="static" sx={{mb: '30px'}}>
@@ -28,8 +32,10 @@ function App() {
                         </IconButton>
                         <Switch size="small" onChange={changeThemeHandler}/>
                     </Toolbar>
+                    {status === 'loading' && <LinearProgress/>}
+                    {status === 'failed' && <LinearProgress variant="buffer" value={100} color="error"/>}
                 </AppBar>
-                <TodoListsList/>
+                <TodoListsList demo={demo}/>
             </ThemeProvider>
         </div>
     );
