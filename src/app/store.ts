1cb76cee99ae6f6bd8c5daee64f62ac622a414ec
gqtpe <work.temirtas@gmail.com>
2024-07-11 12:26:06 +0500

    todoList v16: authReducer added

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,15 @@ import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'r
 import {tasksReducer} from "../features/TodoListsList/TodoList/tasks-reducer";
 import {todoListsReducer} from "../features/TodoListsList/TodoList/todoLists-reducer";
 import {thunk, ThunkDispatch} from "redux-thunk";
-import {useDispatch} from "react-redux";
-import { appReducer } from './app-reducer';
+import {appReducer} from './app-reducer';
+import {authReducer} from "../features/Login/auth-reducer";
 
 
 const rootReducer = combineReducers({
     todoLists: todoListsReducer,
     tasks: tasksReducer,
     app: appReducer,
+    auth: authReducer,
 })
 export const store = legacy_createStore(rootReducer, undefined, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
