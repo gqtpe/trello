4ac3e132f6d036a9d1b148c6d73105f1a3c8e4f1
gqtpe <work.temirtas@gmail.com>
2024-07-11 12:30:11 +0500

    todoList v16: moved app logic into custom hook

---
+++ src/app/hooks/useAuth.ts
@@ -0,0 +1,19 @@
+import {initializeAppTC, logoutTC} from "../../features/Login/auth-reducer";
+import {useAppDispatch, useAppSelector} from "../store";
+import {useCallback, useEffect} from "react";
+
+
+export const useAuth = () => {
+    console.log('App is called')
+    const isInitialized = useAppSelector(state => state.app.isInitialized)
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [dispatch])
+    const status = useAppSelector(state => state.app.status)
+
+    const logout = useCallback(()=>{
+        dispatch(logoutTC())
+    },[])
+    return {status, isInitialized,logout}
+}
