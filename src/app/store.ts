84a4696985ccd1ab5443c555f696622453246d08
gqtpe <work.temirtas@gmail.com>
2024-07-11 12:26:27 +0500

    todoList v16: useAppSelector custom hook added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,7 @@
 import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
 import {tasksReducer} from "../features/TodoListsList/TodoList/tasks-reducer";
 import {todoListsReducer} from "../features/TodoListsList/TodoList/todoLists-reducer";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {thunk, ThunkDispatch} from "redux-thunk";
 import {appReducer} from './app-reducer';
 import {authReducer} from "../features/Login/auth-reducer";
@@ -18,5 +19,6 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 // @ts-ignore
 window.store = store
