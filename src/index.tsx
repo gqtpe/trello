b7fe1fd91f2f6e683a42d169aba01d5173dcb125
gqtpe <work.temirtas@gmail.com>
2024-07-11 12:24:36 +0500

    todoList v16: routing added

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,47 @@ import reportWebVitals from './reportWebVitals';
 import App from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
+import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
+import TodoListsList from "./features/TodoListsList/TodoListsList";
+import {Login} from "./features/Login/Login";
+import {ErrorPage} from "./components/ErrorPage/ErrorPage";
+import RequireAuth from "./common/hoc/RequireAuth/RequireAuth";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <Navigate to="/404"/>,
+        children: [
+            //automatically navigate to first children(todolists)
+            {
+                index: true,
+                element: <Navigate to="todolists" replace/>
+            },
+            {
+                path: 'todolists',
+                element: <RequireAuth><TodoListsList/></RequireAuth>
+            },
+            {
+                path: 'login',
+                element: <Login/>
+            }
+        ]
+    },
+    {
+        path:'/404',
+        element: <ErrorPage/>
+    }
+])
+
 root.render(
     <Provider store={store}>
-        <App/>
+        <RouterProvider router={router}/>
     </Provider>
 );
 
