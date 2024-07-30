0cc9e1b5ff46c22268e7d3c17db8c6dd609e0023
gqtpe <work.temirtas@gmail.com>
2024-07-30 10:38:57 +0500

    todoList v19: Move custom hooks to individual files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import {AnyAction, combineReducers} from 'redux'
 import {tasksReducer} from "../features/TodoListsList/TodoList/Task/tasks-reducer";
 import {todoListsReducer} from "../features/TodoListsList/TodoList/todoLists-reducer";
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {thunk, ThunkDispatch} from "redux-thunk";
 import {appReducer} from './app-reducer';
 import {authReducer} from "../features/Auth/auth-reducer";
@@ -23,7 +22,5 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 
-export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 // @ts-ignore
 window.store = store
