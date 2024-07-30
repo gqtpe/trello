f413eac564bba9d3feb952bb1f0e6de232a0cc71
gqtpe <work.temirtas@gmail.com>
2024-07-30 10:05:45 +0500

    todoList v19: Add index.ts for centralizing app exports

---
+++ src/app/index.ts
@@ -0,0 +1,11 @@
+import App from "./App";
+import * as appSelectors from "./selectors";
+import * as appHooks from "./hooks"
+import { store } from "./store";
+
+export {
+    appSelectors,
+    App,
+    appHooks,
+    store,
+}
