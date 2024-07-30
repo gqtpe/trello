7f4ee81a6baedc1951ee16268b6f057c91bac7cd
gqtpe <work.temirtas@gmail.com>
2024-07-30 10:05:20 +0500

    todoList v19: Add index.ts for centralized hook exports

---
+++ src/app/hooks/index.ts
@@ -0,0 +1,10 @@
+import {useActions} from "./useActions";
+import {useAppDispatch} from "./useAppDispatch";
+import {useAppSelector} from "./useAppSelector";
+
+
+export {
+    useAppDispatch,
+    useAppSelector,
+    useActions,
+}
