b21e800624dea76fdd24f85ebcef5d3cc58169a7
gqtpe <work.temirtas@gmail.com>
2024-09-23 12:20:29 +0500

    todolist v20: added async errors handling

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -4,6 +4,11 @@ import {Dispatch} from "redux";
 
 type ErrorUtilsDispatchType = Dispatch<SetAppErrorActionType | SetAppStatusActionType>
 
+type ThunkAPI = {
+    dispatch: (action: any) =>void
+    rejectWithValue: Function
+}
+
 
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: ErrorUtilsDispatchType) => {
     if (data.messages.length) {
@@ -20,4 +25,21 @@ export const handleServerNetworkError = (
 ) => {
     dispatch(setAppError({error: error.message}))
     dispatch(setAppStatus({status:'failed'}))
+}
+
+export const handleAsyncServerAppError = <D>(data: ResponseType<D>, thunkAPI: ThunkAPI, showAlert = false, ) => {
+    if(showAlert){
+        thunkAPI.dispatch(setAppError(data.messages.length?{error: data.messages[0]}:{error: 'Some error occurred'}))
+    }
+    thunkAPI.dispatch(setAppStatus({status:'failed'}))
+    return thunkAPI.rejectWithValue({errors: data.messages, fieldsErrors: data.fieldsErrors})
+}
+
+export const handleAsyncNetworkError = (error: AxiosError, thunkAPI: ThunkAPI, showAlert = false) => {
+    debugger;
+    if(showAlert){
+        thunkAPI.dispatch(setAppError({error: error.message}))
+    }
+    thunkAPI.dispatch(setAppStatus({status:'failed'}))
+    return thunkAPI.rejectWithValue({errors: [error.message], fieldsErrors: undefined})
 }
