42beacc0be1f12650424e7f62a9a9e70dbd4ccc6
gqtpe <work.temirtas@gmail.com>
2024-09-23 12:33:36 +0500

    todolist v20(displaying error): displaying error inside addItemFrom using helper

--- src/features/TodoListsList/hooks/useTodoLists.ts
+++ src/features/TodoListsList/hooks/useTodoLists.ts
@@ -7,6 +7,21 @@ export function useTodoLists(demo: boolean = false) {
     const todoLists = useAppSelector(todoListSelectors.selectTodoLists)
     const tasks = useAppSelector(todoListSelectors.selectTasks)
     const {fetchTodoLists, addTodoList} = useActions(todoListActions)
+
+    const addTodoListHandler = useCallback(async (title: string, helper: AddItemSubmitHelper) => {
+        const action = await addTodoList(title)
+        if (todoListActions.addTodoList.rejected.match(action)) {
+            if (action.payload?.errors[0]) {
+                helper.setError(action.payload?.errors[0])
+            } else {
+                helper.setError('some error')
+            }
+        } else {
+            helper.setValue('')
+        }
+    }, [])
+
+
     useEffect(() => {
         if (demo) {
             return;
