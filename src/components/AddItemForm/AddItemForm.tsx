137642cb01cbe13cf55ab36220954a9d352eadec
gqtpe <work.temirtas@gmail.com>
2024-09-23 11:29:12 +0500

    addded max width to add item form

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import {useAddItemForm} from "./hooks/useAddItemForm";
-import { IconButton ,  TextField } from "@mui/material";
+import {IconButton, TextField} from "@mui/material";
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+import {AddItemSubmitHelper} from "../../utils/types";
 
 type AddItemFormPropsType = {
-    addItem: (title: string) => void
+    addItem: (title: string, helper: AddItemSubmitHelper) => void
     disabled?: boolean
 }
 const AddItemForm: React.FC<AddItemFormPropsType> = (props) => {
     console.log('AddItemForm')
     const {title, error, changeItemHandler, enterKeyPressHandler, addItem} = useAddItemForm(props.addItem)
-    return <>
+    return <div style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        maxWidth: '270px'
+    }}>
         <TextField
             label="Add Item"
             variant={'outlined'}
@@ -22,9 +27,9 @@ const AddItemForm: React.FC<AddItemFormPropsType> = (props) => {
             onChange={changeItemHandler}
             onKeyUp={enterKeyPressHandler}
         />
-        <IconButton disabled={props.disabled}  onClick={addItem} color={'primary'}>
+        <IconButton disabled={props.disabled} onClick={addItem} color={'primary'}>
             <AddCircleOutlinedIcon/>
         </IconButton>
-    </>
+    </div>
 }
 export default React.memo(AddItemForm);
