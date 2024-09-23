c32d675edd00a7a1425f2ea33af6eead47ced7be
gqtpe <work.temirtas@gmail.com>
2024-09-23 12:26:35 +0500

    todolist v20: useEditableSpan custom hook used

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -23,27 +23,11 @@ const EditableSpan = (props: PropsType) => {
         error
     } = useEditableSpan(props.value, props.setValue)
 
-    }
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (error) {
-            setError(null)
-        }
-        setTitle(e.currentTarget.value)
-    }
-    const enterHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
-        if (e.key === 'Enter') {
-            deactivateEditMode()
-        }
-    }
-
-    const onBlurHandler = () => {
-        deactivateEditMode()
-    }
-    if(props.edit){
-        return  <TextField type="text" size={'small'} onKeyDown={enterHandler}
-                           autoFocus={true}
-                           error={Boolean(props.error) || Boolean(error)}
-                           onBlur={onBlurHandler} variant={'standard'} value={title} onChange={onChange}/>
+    if (props.edit) {
+        return <TextField type="text" size={'small'} onKeyDown={enterHandler}
+                          autoFocus={true}
+                          error={Boolean(props.error) || Boolean(error)}
+                          onBlur={onBlurHandler} variant={'standard'} value={title} onChange={onChange}/>
     }
 
     return editMode ?
@@ -53,7 +37,8 @@ const EditableSpan = (props: PropsType) => {
                    onBlur={onBlurHandler} variant={'standard'} value={title} onChange={onChange}
                    inputProps={{style: {textAlign: 'center'}}}/>
         :
-        <span onDoubleClick={activateEditMode}>{props.value.length > 20?props.value.slice(0, 15)+'...':props.value}</span>
+        <span
+            onDoubleClick={activateEditMode}>{props.value.length > 20 ? props.value.slice(0, 15) + '...' : props.value}</span>
 };
 
 export default React.memo(EditableSpan);
