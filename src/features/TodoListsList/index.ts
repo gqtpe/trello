f854e9ec53f75020a2047d694c638dd6dfb10d72
gqtpe <work.temirtas@gmail.com>
2024-09-24 10:40:34 +0500

    todolist v20: added reducers to centralized import (TodoListList feature)

--- src/features/TodoListsList/index.ts
+++ src/features/TodoListsList/index.ts
@@ -1,26 +1,33 @@
 import * as todoListSelectors from './selectors'
-import {slice} from './TodoList/todoLists-reducer'
+import {default as todoListSlice} from './TodoList/todoLists-reducer'
+import {default as tasksSlice} from './TodoList/Task/tasks-reducer'
 import {asyncActions as todoListAsyncActions} from './TodoList/todoLists-reducer'
 import {asyncActions as tasksAsyncActions} from './TodoList/Task/tasks-reducer'
 import {TodoListsList} from './TodoListsList'
 
 
 const todoListActions = {
-    ...slice.actions,
+    ...todoListSlice.actions,
     ...todoListAsyncActions,
 
 }
 
 const tasksActions = {
-    ...tasksAsyncActions
+    ...tasksAsyncActions,
+    ...tasksSlice.actions,
 }
 
+const todoListsReducer = todoListSlice.reducer
+
+const tasksReducer = tasksSlice.reducer
 
 
 export {
     todoListSelectors,
     tasksActions,
     todoListActions,
+    todoListsReducer,
+    tasksReducer,
     TodoListsList,
 
 }
