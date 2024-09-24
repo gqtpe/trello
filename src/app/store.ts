e05e83b5b00dc35321a7d955427406184e31e35c
gqtpe <work.temirtas@gmail.com>
2024-09-24 10:38:48 +0500

    todolist v20: fix import paths

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,12 @@
-import {AnyAction, combineReducers} from 'redux'
-import {tasksReducer} from "../features/TodoListsList/TodoList/Task/tasks-reducer";
-import {todoListsReducer} from "../features/TodoListsList/TodoList/todoLists-reducer";
-import {thunk, ThunkDispatch} from "redux-thunk";
-import {appReducer} from './app-reducer';
-import {authReducer} from "../features/Auth/auth-reducer";
+import {combineReducers} from 'redux'
+import {tasksReducer, todoListsReducer} from "../features/TodoListsList"
+import {thunk} from "redux-thunk";
+import {appReducer} from '../features/Application';
+import {authReducer} from "../features/Auth";
 import {configureStore} from "@reduxjs/toolkit";
 
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
     todoLists: todoListsReducer,
     tasks: tasksReducer,
     app: appReducer,
@@ -17,10 +16,6 @@ export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
 })
-
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
-export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 // @ts-ignore
 window.store = store
