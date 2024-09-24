7aeee002bfc70c7b5884944ff804f9986cc89ec5
gqtpe <work.temirtas@gmail.com>
2024-09-24 10:53:22 +0500

    todolist v21: hashRouter added(for gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
+import {createBrowserRouter, createHashRouter, Navigate, RouterProvider} from 'react-router-dom';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import App from "./app/App";
@@ -43,7 +43,7 @@ export const routes = [
     }
 
 ];
-const router = createBrowserRouter(routes)
+const router = createHashRouter(routes)
 
 root.render(
     <Provider store={store}>
