1847ed68b5992b160f2070e00f9fa66112f7cba3
gqtpe <work.temirtas@gmail.com>
2024-09-25 21:51:10 +0500

    todolist v21(captcha): captcha url selector added

--- src/features/Auth/selectors.ts
+++ src/features/Auth/selectors.ts
@@ -1,3 +1,5 @@
 import {AppRootStateType} from "../../utils/redux-utils";
 
 export const selectIsAuth = (state: AppRootStateType) => state.auth.isAuth
+
+export const selectCaptchaURL = (state: AppRootStateType) =>state.auth.captchaURL
