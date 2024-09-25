b220644fdd0be584a938a884a217b89b1e6367df
gqtpe <work.temirtas@gmail.com>
2024-09-25 21:53:17 +0500

    todolist v21(deploying): moved CaptchaDialog logic to customHook

---
+++ src/features/Auth/Login/Captcha/useCaptcha.ts
@@ -0,0 +1,42 @@
+import {ChangeEvent, KeyboardEvent, useCallback, useState} from "react";
+
+
+export const useCaptcha = (onSubmit: (value:string)=>void, error?:string) =>{
+
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState<string>('')
+    const [errorMessage, setErrorMessage] = useState<string | undefined>(error)
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSubmit = useCallback(() => {
+        if (value.trim() !== '') {
+            handleClose()
+            onSubmit(value)
+        } else {
+            setErrorMessage('Invalid captcha')
+        }
+    },[])
+
+
+    const onChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+        if (errorMessage) {
+            setErrorMessage(undefined)
+        }
+        setValue(e.target.value)
+    },[])
+    const enterKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleSubmit()
+        }
+    }
+
+
+    return {open, value, errorMessage, handleClickOpen, handleClose, handleSubmit, onChange, enterKeyPress}
+}
