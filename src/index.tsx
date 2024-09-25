07472925fab73d65e10dd6b7baa8282258e953cd
gqtpe <work.temirtas@gmail.com>
2024-09-25 21:39:51 +0500

    todolist v21(deploying): turned BrowserRouter to HashRouter(githubpages for)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, createHashRouter, Navigate, RouterProvider} from 'react-router-dom';
+import {createHashRouter, Navigate, RouterProvider} from 'react-router-dom';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import App from "./app/App";
